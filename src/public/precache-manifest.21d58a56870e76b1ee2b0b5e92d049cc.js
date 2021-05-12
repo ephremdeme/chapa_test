23f845450ce300d83e76818fecea1548f5bf20e8
ephremdeme <demelashephrem@gmail.com>
2021-05-13 00:35:11 +0300

    📦 NEW: UI for card management

---
+++ src/public/precache-manifest.21d58a56870e76b1ee2b0b5e92d049cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b8207e24686c4d7b6e0b95054dcbc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5557ad1055f0e5129741",
+    "url": "/static/js/2.12433990.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.12433990.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c416bee4d18124a85520",
+    "url": "/static/js/main.ad6b6259.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
